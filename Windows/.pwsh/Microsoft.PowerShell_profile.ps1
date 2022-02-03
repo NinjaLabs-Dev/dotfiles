c8a06638b83d94b52dd669cc938c534a9f99f86b
Fraser <fraser@ninjalabs.co.uk>
2022-02-03 13:41:33 +0000

    Update Microsoft.PowerShell_profile.ps1

--- Windows/.pwsh/Microsoft.PowerShell_profile.ps1
+++ Windows/.pwsh/Microsoft.PowerShell_profile.ps1
@@ -1,4 +1,5 @@
 New-Alias g goto
+New-Alias lara php_artisan
 
 function goto {
     param (
@@ -24,7 +25,43 @@ function goto {
     }
 }
 
+function php_artisan {
+    param(
+        $cmd,
+        $options
+    )
+
+    Switch($cmd) {
+        "m" {
+            powershell -Command "php artisan migrate"
+        }
+        "fresh" {
+            powershell -Command "php artisan migrate:fresh --seed"
+        }
+        "install" {
+            powershell -Command "composer install"
+            powershell -Command "npm install"
+        }
+        "clear" {
+            switch($options) {
+                "all" {
+                    powershell -Command "php artisan cache:clear"
+                    powershell -Command "php artisan route:clear"
+                    powershell -Command "php artisan config:clear"
+                    powershell -Command "php artisan view:clear"
+                }
+                default {
+                    powershell -Command "php artisan "$options":clear"
+                }
+            }
+        }
+        default {
+            echo "Command not found"
+        }
+    }
+}
+
 $ENV:STARSHIP_CONFIG = "$HOME\.starship\starship.toml"
-$ENV:STARSHIP_DISTRO = "者  $ENV:UserName"
+$ENV:STARSHIP_DISTRO = "者  $ENV:UserName "
 Invoke-Expression (&starship init powershell)
 Import-Module -Name Terminal-Icons
